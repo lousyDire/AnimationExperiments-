91a219e7c18be3c70729ca49d810c2daf42f273d
lousyDire <121050082+lousyDire@users.noreply.github.com>
2025-03-27 22:00:55 +0800

    first commit

---
+++ testUIProject/testUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  testUIProjectApp.swift
+//  testUIProject
+//
+//  Created by Игорь Чикичев on 04.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct testUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
